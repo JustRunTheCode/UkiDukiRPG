6de7b9654fec385bf680f31e3a549c3ee3bf1874
Nemanja <nemanjaradovanovic.dev@gmail.com>
2026-07-10 13:13:12 +0200

    Core | Implement Time System

---
+++ Server/UkiDukiRPG.Core/Domain/Time/Time.cs
@@ -0,0 +1,34 @@
+﻿namespace UkiDukiRPG.Core.Domain.Time;
+
+public enum TimeUnit
+{
+    Tick,
+    Turn,
+    Round,
+}
+
+public readonly struct TimeInterval(int ticks = 0)
+{
+    public const int TickTimeMultiplier  = 1;
+    public const int TurnTimeMultiplier  = TickTimeMultiplier;
+    public const int RoundTimeMultiplier = TurnTimeMultiplier * 2;
+
+    public int Ticks { get; } = ticks;
+
+    public static TimeInterval FromTurns(int turns) => new(TurnTimeMultiplier * turns);
+
+    public static TimeInterval FromRounds(int rounds) => new(RoundTimeMultiplier * rounds);
+
+    public static TimeInterval FromTicks(int ticks) => new(TickTimeMultiplier * ticks);
+
+    public static TimeInterval From(int interval, TimeUnit timeUnit)
+    {
+        return timeUnit switch
+               {
+                   TimeUnit.Tick  => FromTicks(interval),
+                   TimeUnit.Turn  => FromTurns(interval),
+                   TimeUnit.Round => FromRounds(interval),
+                   _              => FromTicks(0)
+               };
+    }
+}
