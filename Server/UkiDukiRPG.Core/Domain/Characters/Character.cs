5f45a920a57848c6f85601f1aeeffc839c1e2f1b
Nemanja <nemanjaradovanovic.dev@gmail.com>
2026-07-13 03:32:08 +0200

    Core | Implement Basic Abstraction Character Fields

---
+++ Server/UkiDukiRPG.Core/Domain/Characters/Character.cs
@@ -0,0 +1,21 @@
+﻿using UkiDukiRPG.Core.Domain.Attributes;
+using UkiDukiRPG.Core.Domain.Leveling;
+using UkiDukiRPG.Core.Domain.Stats;
+
+namespace UkiDukiRPG.Core.Domain.Characters;
+
+public abstract class Character(AttributeSet baseAttributes, StatBlock baseStats)
+{
+    public readonly AttributeSet BaseAttributes      = baseAttributes;
+    public readonly AttributeSet UpgradedAttributes  = new();
+    public readonly AttributeSet EffectiveAttributes = baseAttributes;
+
+    public readonly StatBlock BaseStats      = baseStats;
+    public readonly StatBlock EffectiveStats = baseStats + baseAttributes;
+
+    public readonly Experience Experience = new(268, new LevelRequirements());
+
+    // Battle Affected Properties
+    public AttributeSet InBattleAttributes { get; } = new();
+    public StatBlock    InBattleStats      { get; } = new();
+}
